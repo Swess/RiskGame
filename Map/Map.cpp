8255d88aac3dae9cc3f498f7b83f89adfb944444
Isaac Doré <swessmusic@gmail.com>
2019-09-26 16:49:06 -0400

    Progress with data members as pointers

--- Map/Map.cpp
+++ Map/Map.cpp
@@ -9,23 +9,41 @@ Edge::Edge(Node *a, Node *b) : a(a), b(b) {}
 // Node
 //
 int Node::get_deg() {
-    return incidents_edges.size();
+    return incidents_edges->size();
 }
 
 
 //
 // Country
 //
-string Country::get_name() {
-    return name;
+Country::Country(const string &name) {
+    this->name = new string(name);
+    incidents_edges = new vector<Edge *>;
 }
+
+Country::~Country() {
+    delete this->name;
+    delete this->nb_armies;
+
+    for (int i = 0; i < incidents_edges->size(); ++i) {
+        // Delete each edges
+    }
+
+    // Remove dangling pointers
+    this->name = nullptr;
+    this->nb_armies = nullptr;
+}
+
 
 //
 // Graph
 //
 Graph::Graph() {
-    edges.reserve(10);
-    nodes.reserve(10);
+    edges = new vector<Edge *>;
+    nodes = new vector<Node *>;
+
+    edges->reserve(10);
+    nodes->reserve(10);
 }
 
 bool Graph::are_adjacent(Node &a, Node &b) {
@@ -33,17 +51,17 @@ bool Graph::are_adjacent(Node &a, Node &b) {
     Node searching = b;
 
     // Iterate only for lowest degree node
-    if(b.get_deg() < a.get_deg()){
+    if (b.get_deg() < a.get_deg()) {
         testing = b;
         searching = a;
     }
 
     // Check all incidences
-    for(int i=0; i<testing.get_deg(); i++){
-        Edge* e = testing.incidents_edges.at(i);
+    for (int i = 0; i < testing.get_deg(); i++) {
+        Edge *e = testing.incidents_edges->at(i);
 
         // Comparing pointers
-        if(e->a == &searching || e->b == &searching){
+        if (e->a == &searching || e->b == &searching) {
             return true;
         }
     }
@@ -52,24 +70,41 @@ bool Graph::are_adjacent(Node &a, Node &b) {
 }
 
 void Graph::insert_node(Node &new_node) {
-    Node* node_ptr = &new_node;
-    nodes.push_back(node_ptr);
+    Node *node_ptr = &new_node;
+    nodes->push_back(node_ptr);
 }
 
-Edge* Graph::insert_edge(Node &a, Node &b) {
-    Edge* edge_ptr = new Edge(&a, &b);
-    edges.push_back(edge_ptr);
+Edge *Graph::insert_edge(Node &a, Node &b) {
+    Edge *edge_ptr = new Edge(&a, &b);
+    edges->push_back(edge_ptr);
     return edge_ptr;
+}
+
+vector<Edge *> Graph::get_edges() {
+    return *edges;
+}
+
+vector<Node *> Graph::get_nodes() {
+    return *nodes;
 }
 
 //
 // Continent
 //
 int Continent::id_count = 0;
-string Continent::get_name() {
-    return name;
+
+Continent::Continent(string *name) : name(name) {
+    id = new int(++id_count);
 }
 
-Continent::Continent(const string &name) : name(name) {
-    id = ++id_count;
+
+//
+// Map
+//
+void Map::add_country(const Country *new_country) {
+    graph->insert_node((Node &) *new_country);
+}
+
+void Map::connect(const Country *a, const Country *b) {
+    graph->insert_edge((Node &) *a, (Node &) *b);
 }
--- Map/Map.h
+++ Map/Map.h
@@ -1,59 +1,71 @@
 #pragma once
+
 #include <vector>
 #include <string>
 
 using namespace std;
 
-class Edge;
+struct Edge;
 
 struct Node {
-    vector<Edge *> incidents_edges;
-public:
+    vector<Edge *> *incidents_edges;
+
     int get_deg();
 };
 
 struct Edge {
-public:
     Node *a;
     Node *b;
+
     Edge(Node *a, Node *b);
 };
 
 class Country : public Node {
-    string name = "";
 public:
-    string get_name();
+    string *name;
+    int *nb_armies;
+    // TODO: Add the owner player of this country
+
+
+    Country(const string &name);
+
+    virtual ~Country();
 };
 
 class Graph {
 protected:
-    vector<Node *> nodes;
-    vector<Edge *> edges;
+    vector<Node *> *nodes;
+    vector<Edge *> *edges;
 public:
     Graph();
-
-    void merge_subgraph(const Graph &to_merge_in);
 
     void insert_node(Node &new_node);
 
     Edge *insert_edge(Node &a, Node &b);
 
     bool are_adjacent(Node &a, Node &b);
+
+    vector<Edge *> get_edges();
+
+    vector<Node *> get_nodes();
 };
 
 class Continent : public Graph {
     static int id_count;
-    int id;
-    string name = "";
+    int *id;
+    string *name;
+
 public:
-    Continent(const string &name);
-
-    string get_name();
-
-    vector<Country *> get_countries();
+    Continent(string *name);
 };
 
 class Map {
+public:
     Graph *graph;
 
+    void add_continent(const Graph *continent);
+
+    void add_country(const Country *new_country);
+
+    void connect(const Country *a, const Country *b);
 };
