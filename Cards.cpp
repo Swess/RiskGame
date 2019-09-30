ec8690511467959ea12427cdbde49f0afad74424
Nwitz <Foolzo13@gmail.com>
2019-09-29 21:11:19 -0400

    Deck functionality

--- Cards.cpp
+++ Cards.cpp
@@ -3,4 +3,67 @@
 //
 
 #include "Cards.h"
+#include <algorithm>
+#include <iostream>
 
+using namespace std;
+
+// Card functions
+
+
+Card::Card() {
+
+}
+
+Card::Card(string*, Card::Type*) {
+
+}
+
+// Deck Functions
+
+
+// Card functions
+Deck::Deck(string countries[], int *numCountries) {
+    size = numCountries;
+    cards = new Card[*numCountries];
+    *currentCardIndex = 0;
+    createCards(countries);
+    shuffleDeck();
+}
+
+void Deck::createCards(string countries[]) {
+    auto *currentType = new Card::Type;
+    int starting = rand() % 3;
+    for (int i = 0; i < *size; i++){
+        *currentType = Card::Type((starting + i) % 3);
+        cards[i] = Card(&countries[i], currentType);
+    }
+}
+
+void Deck::shuffleDeck() {
+    int *tempCardIndex = new int;
+    for (int i = 0; i < *size; i++) {
+        *tempCardIndex = random() % *size;
+        swap(cards[i], cards[*tempCardIndex]);
+    }
+}
+
+Card *Deck::draw() {
+    if (*currentCardIndex == *size) {
+        cout << "no more cards remaining";
+        return nullptr;
+    } else {
+        return &cards[*currentCardIndex++];
+    }
+}
+
+// Deck functions
+// Hand functions
+
+
+Hand::Hand() {
+
+}
+
+
+// Hand functions
--- Cards.h
+++ Cards.h
@@ -5,4 +5,46 @@
 #ifndef TEAM12_DOMINATIONGAME_CARDS_H
 #define TEAM12_DOMINATIONGAME_CARDS_H
 
+#pragma once
+#include <string>
+
+using namespace std;
+
+class Card {
+public:
+    enum Type {infantry = 0, artillery = 1, cavalry = 2};
+    Card();
+    Card(string*, Type*);
+
+private:
+    string *country;
+    Type *type;
+};
+
+
+class Deck {
+public:
+    Deck(string countries[], int *size);
+    Card* draw();
+
+private:
+    int *size;
+    Card *cards; // Array of cards
+    int *currentCardIndex;
+    void shuffleDeck();
+
+    void createCards(string *countries);
+};
+
+class Hand {
+public:
+    Hand();
+    Card::Type *exchange();
+
+private:
+    Card* cards;
+};
+
+
+
 #endif //TEAM12_DOMINATIONGAME_CARDS_H
