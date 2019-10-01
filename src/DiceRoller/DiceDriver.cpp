b4611bc90c047d3d7608d7d5ed6727f86e3888d8
Francesco Benzi <97derkanadier@gmail.com>
2019-10-01 15:27:28 -0400

    Final Commit After Merging + CLion Working :)

--- src/DiceRoller/DiceDriver.cpp
+++ src/DiceRoller/DiceDriver.cpp
@@ -23,46 +23,58 @@ void printRollSorter(vector<int> &vec){
     cout<< endl;
 }
 
-int main(/*int argc, const char * argv[]*/) {
-    srand(time(0)); // Setting a unique pattern for the rand() function
-    
-    /*
-       From the outline:
-    
-       Driver that test: 1) one can request from 1 to 3 dice being rolled, 2) that the container returns the right number of values in a sorted container, 3) that no value outside of the 1-6 range is ever returned, 4) that there is an equal share of 1-6 values returned and 5) that every dice rolling facility object maintains a percentage of all individual values rolled up to now.
-    */
-    
-    //Test Die
-    Dice object_1;
-    vector<int> rollSorter_1;
-    
-    /*
-     * The variable 'iterations' is used to select how many iterations to test (at least 3 in the assignment) but for instance 10
-     * to 500 will show that the % for each dice face value will be approximately ~16% = 1/6 = fair dice / equal share
-     */
-    int iterations = 500;
-    
-    //This variable defines the number of Dice to roll (from 1 to 3 in the Risk Game)
-    int number_of_dice_rolled = 3;
-    
-    
-    cout << "\nWelcome to DICE tester" << endl;
-    
-    //This do-while cycle tests the requested amount of iterations of the diceRoller function
-    do {
-        
-        rollSorter_1 = object_1.diceRoller(number_of_dice_rolled);    //creates a rollsorter vector with the Dice value provided
-        cout << "\nrollSorter values: ";
-        printRollSorter(rollSorter_1);                                   //prints the values generated in rollSorter_1
-        cout << "\nRolling percentages: " << endl;
-        object_1.printRollPercentageArray();
-        
-        iterations--;
-        
-    }while(iterations > 0);
-    
-    return 0;
+namespace DiceDriver {
+
+    int run () {
+
+        srand(time(0)); // Setting a unique pattern for the rand() function
+
+        /*
+           From the outline:
+
+           Driver that test:
+           1) one can request from 1 to 3 dice being rolled,
+           2) that the container returns the right number of values in a sorted container,
+           3) that no value outside of the 1-6 range is ever returned,
+           4) that there is an equal share of 1-6 values returned and
+           5) that every dice rolling facility object maintains a percentage of all individual values rolled up to now.
+
+        */
+
+        //Test Die
+        Dice object_1;
+        vector<int> rollSorter_1;
+
+        /*
+         * The variable 'iterations' is used to select how many iterations to test (at least 3 in the assignment) but for instance 10
+         * to 500 will show that the % for each dice face value will be approximately ~16% = 1/6 = fair dice / equal share
+         */
+        int iterations = 100;
+
+        //This variable defines the number of Dice to roll (from 1 to 3 in the Risk Game)
+        int number_of_dice_rolled = 3;
+
+
+        cout << "\nWelcome to DICE tester" << endl;
+
+        //This do-while cycle tests the requested amount of iterations of the diceRoller function
+        do {
+
+            rollSorter_1 = object_1.diceRoller(number_of_dice_rolled);    //creates a rollsorter vector with the Dice value provided
+            cout << "\nrollSorter values: ";
+            printRollSorter(rollSorter_1);                                   //prints the values generated in rollSorter_1
+            cout << "\nRolling percentages: " << endl;
+            object_1.printRollPercentageArray();
+
+            iterations--;
+
+        }while(iterations > 0);
+
+        return 0;
+    }
+
 }
+
 
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include "Map/MapDriver.h"
+#include "DiceRoller/DiceDriver.h"
+
 using namespace std;
 
 int main() {
     cout << "Domination Game - Team 12 - COMP 345" << endl;
 
     Map::Driver::run();
+    DiceDriver::run();
 
     return 0;
 }
