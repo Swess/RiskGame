dcc8fe48b6920467c5b50c33899db2e173167078
Isaac Doré <swessmusic@gmail.com>
2019-09-30 22:50:56 -0400

    Reimplement in a simpler & more efficient manner

--- src/Map/Map.cpp
+++ src/Map/Map.cpp
@@ -1,364 +1,172 @@
 #include "Map.h"
-#include <iostream>
 #include <list>
-#include <algorithm>
 
 namespace Map {
-    //
-    // Edge
-    //
-    Edge::Edge(Node *a, Node *b) : a(a), b(b) {}
 
-    Node *Edge::opposite(const Node &node) {
-        if (&node == a)
-            return b;
-        if (&node == b)
-            return a;
-
-        return nullptr;
-    }
-
-    Edge::~Edge() {
-        a = nullptr;
-        b = nullptr;
-    }
-
-    ContinentalEdge::ContinentalEdge(Node *a, Node *b, Continent *c1, Continent *c2) : Edge(a, b), c1(c1), c2(c2) {}
-
-    Continent *ContinentalEdge::opposite(const Continent &continent) {
-        if (&continent == c1)
-            return c2;
-        if (&continent == c2)
-            return c1;
-
-        return nullptr;
-    }
-
-
-    //
-    // Node
-    //
-    int Node::get_deg() {
-        return incidents_edges->size();
-    }
-
-    Node::Node() { incidents_edges = new vector<Edge *>; }
-
-    Node::~Node() {
-        delete incidents_edges;
-    }
-
-
-    //
-    // Country
-    //
     Country::Country(const string &name) {
         this->name = new string(name);
+        this->index = new int(-1);
+        this->continent_index = new int(-1);
         this->nb_armies = new int;
-        incidents_edges = new vector<Edge *>;
-        this->continent = nullptr;
     }
 
     Country::~Country() {
+        delete index;
         delete name;
+        delete continent_index;
         delete nb_armies;
-        delete incidents_edges;
 
         // Remove dangling pointers
+        index = nullptr;
         name = nullptr;
+        continent_index = nullptr;
         nb_armies = nullptr;
-        incidents_edges = nullptr;
     }
 
-
-    //
-    // Graph
-    //
-    Graph::Graph() {
-        edges = new vector<Edge *>;
-        nodes = new vector<Node *>;
-
-        edges->reserve(10);
-        nodes->reserve(10);
-    }
-
-    bool Graph::are_adjacent(Node &a, Node &b) {
-        Node *testing = &a;
-        Node *searching = &b;
-
-        // Iterate only for lowest degree node
-        if (b.get_deg() < a.get_deg()) {
-            testing = &b;
-            searching = &a;
-        }
-
-        // Check all incidences
-        for (int i = 0; i < testing->get_deg(); i++) {
-            Edge *e = testing->incidents_edges->at(i);
-
-            // Comparing pointers
-            if (e->opposite(*testing) == searching)
-                return true;
-        }
-
-        return false;
-    }
-
-    void Graph::insert_node(Node &new_node) {
-        Node *node_ptr = &new_node;
-        nodes->push_back(node_ptr);
-    }
-
-    Edge *Graph::insert_edge(Node &a, Node &b) {
-        Edge *edge_ptr = new Edge(&a, &b);
-
-        // Add to graph list
-        edges->push_back(edge_ptr);
-
-        // Add to incidents edge list
-        a.incidents_edges->push_back(edge_ptr);
-        b.incidents_edges->push_back(edge_ptr);
-
-        return edge_ptr;
-    }
-
-    vector<Edge *> Graph::get_edges() const {
-        return *edges;
-    }
-
-    vector<Node *> Graph::get_nodes() const {
-        return *nodes;
-    }
-
-    bool Graph::remove_node(Node &node) {
-        // Find it
-        vector<Node *>::iterator it = find(nodes->begin(), nodes->end(), &node);
-        if (it != nodes->end()) {
-
-            // Remove all edge connected to it
-            for (Edge *edge : *node.incidents_edges)
-                this->remove_edge(*edge);
-
-            nodes->erase(it);
-            return true;
-        }
-        return false;
-    }
-
-    bool Graph::remove_edge(Edge &edge) {
-        // Freaking ugly.. but no choices for now... all vectors should have been a custom linked list...
-        vector<Edge *>::iterator it = find(edges->begin(), edges->end(), &edge);
-        if (it != edges->end()) {
-            vector<Edge *> *incA = edge.a->incidents_edges;
-            vector<Edge *> *incB = edge.b->incidents_edges;
-
-            incA->erase(find(incA->begin(), incA->end(), &edge));
-            incB->erase(find(incB->begin(), incB->end(), &edge));
-
-            delete &edge;
-            edges->erase(it);
-            return true;
-        }
-        return false;
-    }
-
-    Graph::~Graph() {
-        for (auto &node : *nodes)
-            delete node;
-
-        for (auto &edge : *edges)
-            delete edge;
-
-        delete nodes;
-        delete edges;
-    }
-
-    bool Graph::is_connected() {
-        // BFS to check if we are able to visit each nodes
-        int size = nodes->size();
-        if (size < 2) return true;
-
-        Node *current = nodes->front();
-        vector<Node *> *visited = new vector<Node *>;
-
-        list<Node *> queue;
-        visited->insert(visited->begin(), current);
-        queue.push_back(current);
-
-        // Iterate
-        vector<Edge *>::iterator i;
-        while (!queue.empty()) {
-            current = queue.front();
-            queue.pop_front();
-
-            for (i = current->incidents_edges->begin(); i != current->incidents_edges->end(); i++) {
-                Node *opposite = (*i)->opposite(*current);
-
-                // If not visited
-                if (opposite && find(visited->begin(), visited->end(), opposite) == visited->end()) {
-                    visited->push_back(opposite);
-                    queue.push_back(opposite);
-                }
-            }
-        }
-
-        return nodes->size() == visited->size();
-    }
-
-    //
-    // Continent
-    //
     Continent::Continent(const string &name) {
-        incidents_edges = new vector<ContinentalEdge *>;
+        this->index = new int(-1);
+        this->size = new int(0);
         this->name = new string(name);
-    }
-
-    void Continent::insert_country(Country &country) {
-        // If already part of a continent
-        if (country.continent)
-            country.continent->remove_node(country);
-
-        this->insert_node(country);
-        country.continent = this;
-    }
-
-    void Continent::remove_country(Country &country) {
-        this->remove_node(country);
+        this->map = nullptr;
     }
 
     Continent::~Continent() {
-        delete incidents_edges;
+        delete index;
+        delete size;
         delete name;
     }
 
+    void Continent::insert_country(Country &country) {
+        map->insert_country(country);
+        map->connect(*this, country);
+    }
+
     bool Continent::is_connected() {
-        // BFS to check if we are able to visit each nodes of the continent only
-        int size = nodes->size();
-        if (size < 2) return true;
+        if(*size < 2) return true;
+        int current = -1;
+        int visited_count = 0;
 
-        Node *current = nodes->front();
-        vector<Node *> *visited = new vector<Node *>;
+        // Finding first country in this continent
+        vector<Country> countries = map->get_countries();
+        for(unsigned i=0; i<countries.size(); i++){
+            if(countries[i].continent_index == index)
+                current = *countries[i].index;
+        }
 
-        list<Node *> queue;
-        visited->insert(visited->begin(), current);
+        if(current == -1) return false;     // Should never happen... Something went wrong
+
+        vector<bool> visited(countries.size(), false);
+
+        list<int> queue;
+        visited[current] = true;
         queue.push_back(current);
 
-        // Iterate
-        vector<Edge *>::iterator i;
-        while (!queue.empty()) {
+        vector<int>::iterator it;
+        while(!queue.empty()){
             current = queue.front();
             queue.pop_front();
 
-            for (i = current->incidents_edges->begin(); i != current->incidents_edges->end(); i++) {
-                Country *opposite = (Country *)(*i)->opposite(*current);
-
-                // If not visited (Continent only)
-                if (opposite && opposite->continent == this &&
-                    find(visited->begin(), visited->end(), opposite) == visited->end()) {
-                    visited->push_back(opposite);
-                    queue.push_back(opposite);
+            // Adjacents of current visited node
+            for(it = map->edges->at(current).begin(); it!=map->edges->at(current).end(); it++){
+                if(!visited[*it]){
+                    visited[*it] = true;
+                    queue.push_back(*it);
+                    visited_count++;
                 }
             }
         }
 
-        return nodes->size() == visited->size();
+        // We visited everyone of them ?
+        return visited_count == *size;
     }
 
-
-    //
-    // Map
-    //
-    void Map::insert_continent(Continent *continent) {
-        continents->push_back(continent);
-    }
 
     Map::Map() {
-        continents = new vector<Continent *>;
-        edges = new vector<ContinentalEdge *>;
+        continents = new vector<Continent>;
+        countries = new vector<Country>;
+        edges = new vector<vector<int>>;
     }
 
     Map::~Map() {
-        for (auto &edge : *edges)
-            delete edge;
-
         delete continents;
+        delete countries;
         delete edges;
     }
 
-    ContinentalEdge* Map::connect_continents(Country &country1, Country &country2) {
-        if( !country1.continent || !country2.continent ){
-            cout << "Could not connect countries since one/both of them is not part of a continent." << endl;
-            return nullptr;
+    void Map::insert_country(Country &new_node) {
+        *new_node.index = countries->size();
+        countries->push_back(new_node);
+
+        // Create edges mem loc
+        edges->push_back(*new vector<int>);
+    }
+
+    void Map::connect(const Country &a, const Country &b) {
+        // Define direction, a to b
+        edges->at(*a.index).push_back(*b.index);
+    }
+
+    bool Map::are_adjacent(const Country &a, const Country &b) {
+        return are_adjacent(*a.index, *b.index);
+    }
+
+    bool Map::are_adjacent(int &a, int &b) {
+        vector<int> links = edges->at(a);
+
+        for (unsigned i = 0; i < links.size(); i++) {
+            if (links.at(i) == b)
+                return true;
         }
+        return false;
+    }
 
-        if( country1.continent == country2.continent ){
-            cout << "Cannot connect 2 countries that are part of the same continent. Use the Continent Graph itself." << endl;
-            return nullptr;
-        }
+    vector<Country> Map::get_countries() const {
+        return *countries;
+    }
 
-        if(
-            find(continents->begin(), continents->end(), country1.continent) == continents->end()
-            || find(continents->begin(), continents->end(), country2.continent) == continents->end()
-        ){
-            cout << "Cannot connect countries which continent is not part of the map." << endl;
-            return nullptr;
-        }
+    vector<Continent> Map::get_continents() const {
+        return *continents;
+    }
 
-        auto *edge_ptr = new ContinentalEdge(&country1, &country2, country1.continent, country2.continent);
+    void Map::connect(Continent &cont, Country &country) {
+        if (*country.continent_index != -1)
+            continents->at(*country.continent_index).size--;
 
-        // Add to graph list
-        edges->push_back(edge_ptr);
-
-        // Add to incidents edge list
-        country1.incidents_edges->push_back(edge_ptr);
-        country1.continent->incidents_edges->push_back(edge_ptr);
-        country2.incidents_edges->push_back(edge_ptr);
-        country2.continent->incidents_edges->push_back(edge_ptr);
-
-        return edge_ptr;
+        country.continent_index = cont.index;
+        cont.size++;
     }
 
     bool Map::is_connected() {
-        // Check if all continent are connected themselves internally
-        for(int i=0; i<continents->size(); i++){
-            if (!continents->at(i)->is_connected())
-                return false;
-        }
+        if(countries->size() < 2) return true;
 
-        // Check if all continents are interconnected, using ContinentalEdges
-        // BFS to check if we are able to visit each continent
-        int size = continents->size();
-        if (size < 2) return true;
+        int current = 0;
+        vector<bool> visited(countries->size(), false);
 
-        Continent *current = continents->front();
-        auto *visited = new vector<Continent *>;
-
-        list<Continent *> queue;
-        visited->insert(visited->begin(), current);
+        list<int> queue;
+        visited[current] = true;
         queue.push_back(current);
 
-        // Iterate
-        vector<ContinentalEdge *>::iterator i;
-        while (!queue.empty()) {
+        vector<int>::iterator it;
+        while(!queue.empty()){
             current = queue.front();
             queue.pop_front();
 
-            for (i = current->incidents_edges->begin(); i != current->incidents_edges->end(); i++) {
-                Continent *opposite = (*i)->opposite(*current);
-
-                // If not visited
-                if (opposite && find(visited->begin(), visited->end(), opposite) == visited->end()) {
-                    visited->push_back(opposite);
-                    queue.push_back(opposite);
+            // Adjacents of current visited node
+            for(it = edges->at(current).begin(); it!=edges->at(current).end(); it++){
+                if(!visited[*it]){
+                    visited[*it] = true;
+                    queue.push_back(*it);
                 }
             }
         }
 
-        return continents->size() == visited->size();
-    }
+        // Check if all visited
+        for(unsigned i=0; i<visited.size(); i++){
+            if(!visited[i])
+                return false;
+        }
 
-    vector<Continent *> Map::get_continents() {
-        return *continents;
+        return true;
     }
 
 
--- src/Map/Map.h
+++ src/Map/Map.h
@@ -6,111 +6,63 @@
 using namespace std;
 
 namespace Map {
-    struct Edge;
 
-    class Continent;
+    class Map;
 
-    struct Node {
-        vector<Edge *> *incidents_edges;
-
-        Node();
-
-        ~Node();
-
-        int get_deg();
-    };
-
-    struct Edge {
-        Node *a;
-        Node *b;
-
-        Edge(Node *a, Node *b);
-
-        ~Edge();
-
-        Node *opposite(const Node &node);
-    };
-
-    class ContinentalEdge : public Edge {
-        Continent *c1, *c2;
+    class Country {
     public:
-        ContinentalEdge(Node *a, Node *b, Continent *c1, Continent *c2);
-
-        Continent *opposite(const Continent &continent);
-    };
-
-    class Country : public Node {
-    public:
+        int *index;
         string *name;
+        int *continent_index;
         int *nb_armies;
-        Continent *continent;
         // TODO: Add the owner player of this country
 
         Country(const string &name);
 
-        virtual ~Country();
+        ~Country();
     };
 
-    class Graph {
-    protected:
-        vector<Node *> *nodes;
-        vector<Edge *> *edges;
+    class Continent {
     public:
-        Graph();
-
-        virtual ~Graph();
-
-        void insert_node(Node &new_node);
-
-        bool remove_node(Node &node);
-
-        Edge *insert_edge(Node &a, Node &b);
-
-        bool remove_edge(Edge &edge);
-
-        bool are_adjacent(Node &a, Node &b);
-
-        vector<Edge *> get_edges() const;
-
-        vector<Node *> get_nodes() const;
-
-        virtual bool is_connected();
-    };
-
-    class Continent : public Graph {
-    private:
-        // Name hiding specific method. Should be using insert_country instead
-        using Graph::insert_node;
-    public:
-
-        vector<ContinentalEdge *> *incidents_edges;
+        int *index;
+        int *size;
         string *name;
+        Map *map;
 
         Continent(const string &name);
 
-        virtual ~Continent();
+        ~Continent();
 
         void insert_country(Country &country);
 
-        void remove_country(Country &country);
-
-        bool is_connected() override;
+        bool is_connected();
     };
 
     class Map {
-        vector<Continent *> *continents;
-        vector<ContinentalEdge *> *edges;
+        vector<Continent> *continents;
+        vector<Country> *countries;
     public:
+        vector<vector<int>> *edges;
+
         Map();
 
         ~Map();
 
-        void insert_continent(Continent *continent);
+        void insert_country(Country &new_node);
 
-        ContinentalEdge *connect_continents(Country &country1, Country &country2);
+        void connect(const Country &a, const Country &b);
 
-        vector<Continent *> get_continents();
+        void connect(Continent &cont, Country &country);
+
+        bool are_adjacent(const Country &a, const Country &b);
+
+        bool are_adjacent(int &a, int &b);
+
+        vector<Country> get_countries() const;
+
+        vector<Continent> get_continents() const;
 
         bool is_connected();
     };
+
 }
