e8f8d39f451f8edbd9c81129009121bc7a780a06
Kerry Gougeon Ducharme <kerrygougeon@gmail.com>
2019-10-23 16:55:55 -0400

    GameEngine skeleton

---
+++ src/GameEngine/GameEngine.h
@@ -0,0 +1,14 @@
+//
+// Created by ker10 on 2019-10-23.
+//
+
+#ifndef TEAM12_DOMINATIONGAME_GAMEENGINE_H
+#define TEAM12_DOMINATIONGAME_GAMEENGINE_H
+
+
+class GameEngine {
+
+};
+
+
+#endif //TEAM12_DOMINATIONGAME_GAMEENGINE_H
---
+++ src/GameEngine/GameEngineDriver.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ker10 on 2019-10-23.
+//
+
+#include "GameEngineDriver.h"
