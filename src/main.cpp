cb9b087d9b208257c97321b302e4d8048e632ed7
Kerry Gougeon Ducharme <kerrygougeon@gmail.com>
2019-11-13 15:52:26 -0500

    read argv and no need to build to switch automaated startup

--- src/main.cpp
+++ src/main.cpp
@@ -12,27 +12,46 @@
 
 using namespace std;
 
-int main() {
+int main(int argc, const char*argv[]) {
 
     Terminal::print("Domination Game - Team 12 - COMP 345");
 
-    bool test = true; // Maybe add a way to read sys args?
+    bool test = false;
+
+    for (int i = 0; i < argc; i++)
+    {
+        string string_test = "test";
+        if (string_test == argv[i] ) test = true;
+    }
+
 
     // Terminal::debug_mode_on();
     if (test) {
         Terminal::test_mode_on();
-//        Terminal::run_test("MapLoader", MapLoader::Driver::run);
-//        Terminal::run_test("Map", Board::Driver::run);
-//        Terminal::run_test("Dice", Dice::Driver::run);
-//        Terminal::run_test("Cards", Cards::Driver::run);
-//        Terminal::run_test("Player", Player::Driver::run);
-//        Terminal::run_test("GameEngine", GameEngine::Driver::run);
+        Terminal::run_test("MapLoader", MapLoader::Driver::run);
+        Terminal::run_test("Map", Board::Driver::run);
+        Terminal::run_test("Dice", Dice::Driver::run);
+        Terminal::run_test("Cards", Cards::Driver::run);
+        Terminal::run_test("Player", Player::Driver::run);
+        Terminal::run_test("GameEngine", GameEngine::Driver::run);
         Terminal::run_test("PlayerStrategies", PlayerStrategies::Driver::run);
         Terminal::test_mode_off();
     } else {
-        GameEngine::GameEngine::instance()->start();
-        GameEngine::GameEngine::instance()->startup_phase();
-        GameEngine::GameEngine::instance()->game_loop();
+        auto gameInstance = GameEngine::GameEngine::instance();
+        int debug_mode = Terminal::print_select("Do you want debug mode on?");
+        if (debug_mode) Terminal::debug_mode_on();
+        int automated_start = Terminal::print_select("Do you want an automated start set-up?");
+        if (automated_start){
+            int players = Terminal::print_select(2,6, "How many players?");
+            Terminal::test_mode_on();
+            gameInstance->start_test(7 , players);
+            gameInstance->startup_phase();
+            Terminal::test_mode_off();
+        } else {
+            gameInstance->start();
+            gameInstance->startup_phase();
+        }
+        gameInstance->game_loop();
     }
 
     return 0;
